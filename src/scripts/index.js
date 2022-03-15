8b2171bb2adb3790470005b52a4161bdad69cc4f
Anoop Elias <anoopelias@gmail.com>
2022-03-15 17:56:49 +0530

    Added bootstrap

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,10 @@
+import 'bootstrap';
 import '../styles/index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { SVG } from '@svgdotjs/svg.js';
 
-const draw = SVG().addTo('body').size(300, 300);
-draw.rect(100, 100).attr({ fill: '#f06' });
+const draw = SVG().addTo('#space').size('100%', '100%');
+draw.rect('100%', '100%').attr({ fill: '#ebfaef' });
 
 console.log('webpack starterkit');
